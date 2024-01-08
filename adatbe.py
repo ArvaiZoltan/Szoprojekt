003c4cb461afd229050028275bee1c38536c573b
ArvaiZoltan <arvaizoltan@kozgekkfhaza.com>
2024-01-08 22:21:24 +0100

    Beolvasás adatbázishoz folytatólagosan bepakol -adatbevitel()- függvényben

--- adatbe.py
+++ adatbe.py
@@ -1,32 +1,30 @@
 import sqlite3
 
+def adatbevitel():
+    adatbázis=sqlite3.connect("próba.db")
 
-adatbázis=sqlite3.connect("próba.db")
+    toll=adatbázis.cursor()
+    toll.execute("CREATE TABLE IF NOT EXISTS tanulók(név, szülév, átlag)")
 
-toll=adatbázis.cursor()
-#toll.execute("DROP TABLE IF EXISTS tanulók")
-toll.execute("CREATE TABLE IF NOT EXISTS tanulók(név, szülév, átlag)")
+    nev=input('Mi legyen a név?')
+    szülev=int(input('Szülév:'))
+    atlag=int(input('Átlag:'))
 
-nev=input('Mi legyen a név?')
-szülev=int(input('Szülév:'))
-atlag=int(input('Átlag:'))
 
-toll.execute(""" INSERT INTO tanulók VALUES
-    ('Mikorka Kálmán', 2000, 2.8),
-    ('Víz Elek', 1995, 4.6) """)
+    adat = (
+        {"név": nev, "szülév": szülev, "átlag":atlag},
+    )
 
-adat = (
-    {"név": nev, "szülév": szülev, "átlag":atlag},
-)
+    toll.executemany("INSERT INTO tanulók VALUES(:név, :szülév, :átlag)", adat)
 
-toll.executemany("INSERT INTO tanulók VALUES(:név, :szülév, :átlag)", adat)
+    adatbázis.commit()
 
-adatbázis.commit()
+    for adatok in toll.execute("SELECT név, szülév FROM tanulók ORDER BY szülév"):
+        print (adatok)
 
-for adatok in toll.execute("SELECT név, szülév FROM tanulók ORDER BY szülév"):
-    print (adatok)
+    adatbázis.close()
 
-adatbázis.close()
+adatbevitel()
     
 
 
