eef4126d7d32983aa9c8daf7a23002f5d1122097
ArvaiZoltan <arvaizoltan@kozgekkfhaza.com>
2024-01-08 22:10:40 +0100

    Beolvasás adatbázishoz berakja az adatbázisba

--- adatbe.py
+++ adatbe.py
@@ -15,6 +15,12 @@ toll.execute(""" INSERT INTO tanulók VALUES
     ('Mikorka Kálmán', 2000, 2.8),
     ('Víz Elek', 1995, 4.6) """)
 
+adat = (
+    {"név": nev, "szülév": szülev, "átlag":atlag},
+)
+
+toll.executemany("INSERT INTO tanulók VALUES(:név, :szülév, :átlag)", adat)
+
 adatbázis.commit()
 
 for adatok in toll.execute("SELECT név, szülév FROM tanulók ORDER BY szülév"):
